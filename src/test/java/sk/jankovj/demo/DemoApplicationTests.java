af39c3be7a84123f72176bbc45b9407b968434ee
jankovj <jaroslav.jankovic@softec.sk>
2024-09-25 10:20:07 +0200

    Failed tests

--- src/test/java/sk/jankovj/demo/DemoApplicationTests.java
+++ src/test/java/sk/jankovj/demo/DemoApplicationTests.java
@@ -3,11 +3,16 @@ package sk.jankovj.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		int value = 1;
+		assertTrue(value > 1);
+
 	}
 
 }
