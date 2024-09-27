be12849a919708e93524d463bcae93ee28119895
jankovj <jaroslav.jankovic@softec.sk>
2024-09-27 08:54:42 +0200

    Fix tests

--- src/test/java/sk/jankovj/demo/DemoApplicationTests.java
+++ src/test/java/sk/jankovj/demo/DemoApplicationTests.java
@@ -3,6 +3,7 @@ package sk.jankovj.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -11,7 +12,7 @@ class DemoApplicationTests {
 	@Test
 	void contextLoads() {
 		int value = 1;
-		assertTrue(value > 1);
+		assertFalse(value > 1);
 
 	}
 
