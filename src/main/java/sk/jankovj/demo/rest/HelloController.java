836d26a29f37ed1cdf3311dde8c6398674546ba6
jankovj <jaroslav.jankovic@softec.sk>
2024-10-04 12:47:48 +0200

    Another try

--- src/main/java/sk/jankovj/demo/rest/HelloController.java
+++ src/main/java/sk/jankovj/demo/rest/HelloController.java
@@ -1,15 +1,19 @@
 package sk.jankovj.demo.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    private Environment env;
+
     @GetMapping("/")
     public String sayHello() {
-        String test_url = System.getenv("TEST_URL");
-
+        String test_url = env.getProperty("test.url");
         return "Hello World!" + " " + test_url;
     }
 }
